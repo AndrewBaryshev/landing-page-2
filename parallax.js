b24be542e1f900f7fdcf8b878487ee224863bb7f
a.baryshev <aa.baryshev@mish.design>
2024-04-03 22:11:40 +0300

    parallax fixed

--- parallax.js
+++ parallax.js
@@ -1,11 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let parallaxElement = document.querySelector('.img-style-smart.parallax-layer');
-  let parallaxWrapper = document.querySelector('.wrapper.parallax');
-
-  function updateParallax() {
-      let scrollPosition = parallaxWrapper.getBoundingClientRect().top;
-      parallaxElement.style.transform = 'translateX(-50%) translateY(' + scrollPosition * 0.5 + 'px)';
-  }
-
-  window.addEventListener('scroll', updateParallax);
+const image = document.getElementById("smart-img-parallax")
+new simpleParallax(image, {
+	overflow: true,
+  scale: 2.5
 });
